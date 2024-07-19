f3c2ac089051585f708da6edec1e861bc518041c
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-07-19 16:47:52 +0900

    Update mac.py

--- mac.py
+++ mac.py
@@ -40,7 +40,7 @@ last_name = ''
 def search_iframe():
     try:
         driver.switch_to.default_content()
-        WebDriverWait(driver, 10).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "searchIframe")))
+        WebDriverWait(driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "searchIframe")))
     except Exception as e:
         print(f"Error switching to iframe: {e}")
 
