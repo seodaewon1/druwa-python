9673608ba9d50c9f1afb411b4b2591f769171a86
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-07-19 17:16:27 +0900

    Update mac.py

--- mac.py
+++ mac.py
@@ -40,14 +40,14 @@ last_name = ''
 def search_iframe():
     try:
         driver.switch_to.default_content()
-        WebDriverWait(driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "searchIframe")))
+        WebDriverWait(driver, 20).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "searchIframe")))
     except Exception as e:
         print(f"Error switching to iframe: {e}")
 
 def entry_iframe():
     try:
         driver.switch_to.default_content()
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="entryIframe"]')))
+        WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="entryIframe"]')))
         driver.switch_to.frame(driver.find_element(By.XPATH, '//*[@id="entryIframe"]'))
     except Exception as e:
         print(f"Error switching to entry iframe: {e}")
