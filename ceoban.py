47c3446eba4fbdb5ba052c1d93148fe4b3840945
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-07-25 19:15:12 +0900

    Update ceoban.py

--- ceoban.py
+++ ceoban.py
@@ -7,54 +7,57 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 from datetime import datetime
-import time
+import os
 import json
 
 # 현재 날짜 가져오기
 current_date = datetime.now().strftime("%Y-%m-%d")
 filename = f"ceoban/ceoban_{current_date}.json"
 
-# 웹드라이버 설치
+# 디렉토리가 존재하지 않으면 생성
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+# 웹드라이버 설정
 options = ChromeOptions()
-options.add_argument("--headless")
+options.add_argument("--headless")  # 화면 출력 없이 실행
 service = ChromeService(executable_path=ChromeDriverManager().install())
 browser = webdriver.Chrome(service=service, options=options)
 
+# 웹 페이지 열기
 browser.get('https://coffeebanhada.com/main/store/find.php?page_now=0&page_row=10&page_num=10&sido=&gugun=&keyword=&s_type4=7#res')
 
 # 페이지가 완전히 로드될 때까지 대기
 WebDriverWait(browser, 10).until(
     EC.presence_of_element_located((By.CLASS_NAME, "box2"))
 )
- 
-# 업데이트된 페이지 소스를 변수에 저장
+
+# 페이지 소스 가져오기
 html_source_updated = browser.page_source
 soup = BeautifulSoup(html_source_updated, 'html.parser')
 
 # 데이터 추출
 ceoban_data = []
 
-# 첫 번째 tracks
+# tbody 내의 모든 tr 요소 선택
 tracks = soup.select("tbody tr")
 
 for track in tracks:
     tds = track.select("td")
-    if len(tds) > 1:  # 두 번째 td가 있는지 확인
+    if len(tds) > 1:
         second_td = tds[1].text.strip()
         third_td = tds[2].text.strip()
 
-            # 이미지 URL 추출
-        image_element = soup.select_one(".fotorama__img")
-        image_url = image_element.get('src') if image_element else None
+        # 이미지 URL 추출 (수정 필요)
+        image_element = track.select_one("img")  # tr 내에서 img 요소 선택
+        image_url = image_element['src'] if image_element else None
+
         ceoban_data.append({
             "title": second_td,
             "address": third_td,
             "img": image_url
         })
 
-print(ceoban_data)
-
-
+# 데이터 확인
 print(ceoban_data)
 
 # 데이터를 JSON 파일로 저장
